a0400bf3187d78acb688c22088f30d5a74bcc458
brandonyach <yachb35@gmail.com>
2025-06-06 08:05:44 -0600

    Consolidate CI workflows, pytest-vcr, remove vcr_config

--- tests/vcr_config.py
+++ tests/vcr_config.py
@@ -1,11 +1,7 @@
 import vcr
 
-
-def vcr_config():
-    return {
-        "record_mode": "new_episodes",
-        "match_on": ["method", "scheme", "host", "port", "path", "query"],
-        "filter_headers": ["authorization", "session-header", "cookie"],
-    }
-    
-vcr.default_vcr = vcr.VCR(**vcr_config())
+vcr = vcr.VCR(
+    record_mode="new_episodes",
+    match_on=["method", "scheme", "host", "port", "path", "query"],
+    filter_headers=["authorization", "session-header", "cookie"],
+)
